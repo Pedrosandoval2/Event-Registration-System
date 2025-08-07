1b84bbc37c080beea9155250555fd09ed901e462
unknown <pedrosandovalespinoza2@gmail.com>
2025-08-07 16:10:47 -0500

    update: get user to search

--- src/components/searchUsersRegistered/SearchUsers.tsx
+++ src/components/searchUsersRegistered/SearchUsers.tsx
@@ -1,25 +1,29 @@
 'use client'
-import { viewStore } from "@/store/viewUsersRegister";
 import { cn } from "@/utils/cn";
 import { ChangeEvent } from "react";
 import { Input } from "@/components/ui/input";
 
-export const SearchUsers = () => {
-  const { addValueSearch } = viewStore()
+interface Props {
+  searchUsers: string;
+  setSearchUsers: (value: string) => void;
+}
+
+export const SearchUsers = ({ searchUsers, setSearchUsers }: Props) => {
 
   const onChange = (e: ChangeEvent<any>) => {
-    addValueSearch(e.target.value)
+    setSearchUsers(e.target.value);
   }
 
   return (
     <div className="mx-auto mb-5 flex   w-1/4">
       <LabelInputContainer>
-        <Input id="inputSearch" placeholder="Buscar...." type="text" onChange={onChange} />
+        <Input id="inputSearch" placeholder="Buscar...." value={searchUsers} type="text" onChange={onChange} />
       </LabelInputContainer>
     </div>
   )
 
 }
+
 const LabelInputContainer = ({
   children,
   className,
